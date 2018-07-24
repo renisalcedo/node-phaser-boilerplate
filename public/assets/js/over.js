17a886333992e5c9b2e2581b1211f1c8a89b06a5
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2018-07-24 17:22:24 -0400

    Removed/Uneeded line

--- public/assets/js/over.js
+++ public/assets/js/over.js
@@ -1,12 +1,13 @@
 const gameOver = {
   create: function() {
-    playState.startResponsive();
-    let overLabel = Game.add.text(80, 150, 'GameOver',
-                                    {font: '30px Courier', fill: '#ffffff'});
-    this.restart();
+    let overLabel = Game.add.text(80, 150, 'GameOver', {
+      font: '30px Courier',
+      fill: '#ffffff'
+    })
+    this.restart()
   },
 
   restart: function() {
-    Game.state.start('menu');
+    Game.state.start('menu')
   }
 }
