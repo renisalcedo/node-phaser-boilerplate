af1f642487869c99647646b6249c401a8711c919
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2018-07-24 17:21:30 -0400

    Added responsiveness

--- public/assets/js/load.js
+++ public/assets/js/load.js
@@ -1,15 +1,21 @@
 const loadState = {
   preload: function() {
     // Loading text with styles
-    let loadingLabel = Game.add.text(80, 150, 'loading...',
-                                    {font: '30px Courier', fill: '#ffffff'});
+    let loadingLabel = Game.add.text(80, 150, 'loading...', {
+      font: '30px Courier',
+      fill: '#ffffff'
+    })
 
     // All preloading assets
-    this.load.image('example', 'assets/images/example.jpg');
+    this.load.image('example', 'assets/images/example.jpg')
   },
 
   create: function() {
+    // ADDS RESPONSIVENESS
+    Game.scale.pageAlignHorizontally = true
+    Game.scale.pageAlignVertically = true
+
     // Call the menu state
-    Game.state.start('menu');
+    Game.state.start('menu')
   }
-};
+}
